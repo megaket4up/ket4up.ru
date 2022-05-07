5592bbb0fe52d14a8eb4725f80cbd93d8cc64c70
megaket4up <megaket4up@gmail.com>
2022-05-07 13:14:45 +0500

    Update configs

--- config/logging.php
+++ config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['dayly'],
             'ignore_exceptions' => false,
         ],
 
